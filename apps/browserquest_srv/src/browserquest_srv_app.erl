0f2dc2dea2e12a3eb2098de846ea559071f2062c
Niclas Axelsson <niclas@burbas.se>
2012-07-07 13:01:07 +0200

    Application skeleton

---
+++ apps/browserquest_srv/src/browserquest_srv_app.erl
@@ -0,0 +1,16 @@
+-module(browserquest_srv_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    browserquest_srv_sup:start_link().
+
+stop(_State) ->
+    ok.
