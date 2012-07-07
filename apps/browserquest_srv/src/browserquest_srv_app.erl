eb9925c9bb4ff45bc7eba804f29ba23bc6f29464
Niclas Axelsson <niclas@burbas.se>
2012-07-07 13:54:06 +0200

    Start cowboy in app file

--- apps/browserquest_srv/src/browserquest_srv_app.erl
+++ apps/browserquest_srv/src/browserquest_srv_app.erl
@@ -10,6 +10,33 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+
+    application:start(lager),
+    lager:set_loglevel(lager_console_backend, debug),
+    
+    %% {Host, list({Path, Handler, Opts})}
+    %% Dispatch the requests (whatever the host is) to
+    %% erws_handler, without any additional options.
+    ListeningPort = 
+	case application:get_env(listening_port) of
+	    undefined ->
+		10100;
+	    {ok, Port} ->
+		Port
+	end,
+
+    Dispatch = [{'_', [
+        {'_', browserquest_srv_handler, []}
+    ]}],
+    
+    lager:debug("Starting browserquest_srv on port ~p", [ListeningPort]),
+
+    %% Name, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts
+    %% Listen in 10100/tcp for http connections.
+    cowboy:start_listener(browserquest_srv_websocket, 100,
+        cowboy_tcp_transport, [{port, ListeningPort}],
+        cowboy_http_protocol, [{dispatch, Dispatch}]
+    ),
     browserquest_srv_sup:start_link().
 
 stop(_State) ->
